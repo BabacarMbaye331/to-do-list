9892ebe7d0d512b32fe3aeef53358ba0123ae89d
Babacar Mbaye <papemb331@gmail.com>
2026-07-09 17:11:54 +0000

    recuperation des fonctions Dashboard et getListesTaches

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// importation des differentsfonctions depuis dom.js
+import { getListesTaches, Dashboard  } from "./dom.js";
+
+// appel a la fonction Dashboard
+Dashboard();
+// appelation de fonction getListesTaches
+getListesTaches();
