a5864fd365152ffe8961f6fe2a3f3cb941487cc5
Babacar Mbaye <papemb331@gmail.com>
2026-07-11 17:49:59 +0000

    ajout des commentaires sur les fonctions de filtrages

--- js/app.js
+++ js/app.js
@@ -15,12 +15,17 @@ getListesTaches(taches);
 search(taches)
 
 // ****************************************fontions filtres
+// fonction du filtrage de tous
 filtre_tous(taches)
 
+// fonction du filtrage des taches en cours
 filtre_EnCours(taches)
 
+// filtrage des taches terminer
 filtre_Terminer(taches)
 
+// filtrage des taches de haute priorites
 HautePriorite(taches)
 
+// filtrage des taches en formation
 FiltreFormation(taches)
