05ecd044e85df395dc9b069cf31529a6d5209c48
Babacar Mbaye <papemb331@gmail.com>
2026-07-11 17:37:22 +0000

    importation des fonctions dashboard et de filtrages

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 // importation des differentsfonctions depuis dom.js
-import { getListesTaches, Dashboard  } from "./dom.js";
+import { getListesTaches, Dashboard, search, filtre_tous, filtre_EnCours, filtre_Terminer, HautePriorite, FiltreFormation } from "./dom.js";
+import { listesTaches } from "./utile.js";
+
+// listes des taches
+const taches = await listesTaches().then(r => { return r });
 
 // appel a la fonction Dashboard
-Dashboard();
+Dashboard(taches);
 // appelation de fonction getListesTaches
-getListesTaches();
+getListesTaches(taches);
+
+//search
+// search(taches);
+search(taches)
+
+// ****************************************fontions filtres
+filtre_tous(taches)
+
+filtre_EnCours(taches)
+
+filtre_Terminer(taches)
+
+HautePriorite(taches)
+
+FiltreFormation(taches)
