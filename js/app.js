bfc354596acdbd592a4cd793aea63d8c146e304b
Babacar Mbaye <papemb331@gmail.com>
2026-07-15 13:04:57 +0000

    definission de la suppression de tache

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 // importation des differentsfonctions depuis dom.js
+import { Storage } from "./storage.js";
 import { getListesTaches, Dashboard, search, filtre_tous, filtre_EnCours, filtre_Terminer, HautePriorite, FiltreFormation } from "./dom.js";
 import { listesTaches } from "./utile.js";
 
 // listes des taches
-const taches = await listesTaches().then(r => { return r });
+let taches = JSON.parse(localStorage.getItem("listes"))
+
+if(!taches) {
+    taches = await listesTaches();
+    localStorage.setItem('listes', JSON.stringify(taches))
+}
 
 // appel a la fonction Dashboard
 Dashboard(taches);
-// appelation de fonction getListesTaches
-getListesTaches(taches);
 
 //search
-// search(taches);
 search(taches)
 
 // ****************************************fontions filtres
@@ -29,3 +32,25 @@ HautePriorite(taches)
 
 // filtrage des taches en formation
 FiltreFormation(taches)
+
+// appel de la fonction Storage
+Storage(taches)
+
+
+
+// fonction de la bouton de supperssion d'une tache
+const btn_suppression = document.querySelectorAll(".btn_suppression");
+btn_suppression.forEach(b => {
+    b.addEventListener('click', (e) => {
+
+        const id_v = Number(e.target.dataset.id);
+
+        const tachesApresSuppression = taches.filter(e => e.id !== id_v);
+
+        localStorage.setItem('listes', JSON.stringify(tachesApresSuppression));
+
+        e.target.closest('.card').remove()
+
+        console.log(id_v)
+    })
+})
